a10450539ef365da509a2800111804dc65acfeea
Imran Sarder <imrancluster@gmail.com>
2018-04-17 18:20:09 +0600

    Added basis laravel scheduler for my this application

---
+++ app/Console/Commands/ActiveUsers.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\SendMailable;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class ActiveUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'active:users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send an email every 15 days to check the member is active.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $totalUsers = \DB::table('users')->count();
+
+        Mail::to('imrancluster@test.com')->send(new SendMailable($totalUsers));
+    }
+}
