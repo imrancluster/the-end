b52bcf9d3daeaece4c156a81be4b665eee57db83
Imran Sarder <imrancluster@gmail.com>
2018-04-22 17:09:58 +0600

    Set TODO for scheduler email to the members

--- app/Console/Commands/ActiveUsers.php
+++ app/Console/Commands/ActiveUsers.php
@@ -46,6 +46,34 @@ class ActiveUsers extends Command
         $livings = [];
 
         foreach ($allLivings as $living) {
+
+            // TODO: if member open the email and click on the link e.g: /living/[token]
+            // If user the token is valid then update send_email_after = 15, last_email_seen = time()
+
+            // TODO: if send_email_after = 15
+            // then check last_email_sent and compare with current time
+            // then send email
+            // update send_email_after = 7
+            // update last_email_sent = time()
+
+            // TODO: if send_email_after = 7
+            // then check last_email_sent and compare with current time
+            // then send email
+            // update send_email_after = 3
+            // update last_email_sent = time()
+
+            // TODO: if send_email_after = 3
+            // then check last_email_sent and compare with current time
+            // then send email
+            // update send_email_after = 1
+            // update last_email_sent = time()
+
+            // TODO: if send_email_after = 1
+            // then check last_email_sent and compare with current time
+            // then send email
+            // update send_email_after = 0
+            // update last_email_sent = time()
+
             $livings[] = [
                 'id' => $living->id,
                 'last_email_sent' => $living->last_email_sent,
@@ -54,7 +82,7 @@ class ActiveUsers extends Command
                 'user' => [
                     'id' => $living->user->id,
                     'name' => $living->user->name,
-                    'emailc' => $living->user->email,
+                    'email' => $living->user->email,
                 ],
             ];
         }
